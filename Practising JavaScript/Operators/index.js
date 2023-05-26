d7296b1599c54f0e0348185dbebfe1e030ae5483
Johnpaul <indisputable.jay@gamil.com>
2023-05-26 08:45:07 +0100

    Adding a sub-folder

---
+++ Practising JavaScript/Operators/index.js
@@ -0,0 +1,16 @@
+let x = 120;
+let value = x;
+
+let type = value > 100 ? 'gold' : 'silver'
+
+// console.log(type);
+
+//Logical Operators
+//Logical AND (&&)
+//Returns true only when both operands are TRUE
+
+let highIncome = true;
+let goodCreditScore = true;
+
+eligibleForLoan = highIncome && goodCreditScore;
+console.log(eligibleForLoan)
