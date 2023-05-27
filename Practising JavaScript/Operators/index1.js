2c1fb4968d04f484fe374bcba5c78eac631cf361
Johnpaul <indisputable.jay@gamil.com>
2023-05-27 18:38:56 +0100

    Operators

--- Practising JavaScript/Operators/index1.js
+++ Practising JavaScript/Operators/index1.js
@@ -3,13 +3,38 @@
 //Using logical operators with Boolean values
 //Examle 1
 let hello = false || true //ture
-console.log(hello)
+console.log(hello);
 
 //Using logical operators with non-Boolean Values
-//Example 1
-let name = false || 'Johnpaul'
+//Example 2
+let name = false || 'Johnpaul' //this is Truthy thats why it returns Johnpaul since we are dealing with logical or
 console.log(name);
 
-//Example 2
-let number = false || 1;
-console.log(number)
+//Example 3
+let number = false || 1; // This is also truthy since its not a falsy and hence JavaScript output 1
+console.log(number);
+
+//If you run example 2 and 3, you'll notice that JavaScript returns the values of the right hand side. They are intrepeted as what is known as truthy & falsy
+//Examples of falsy values include
+// undefined
+// null
+// 0
+// False
+// ' '
+// NaN
+
+//Now anything that is not Falsy --> Truthy
+
+
+//Short Circuiting
+let test = false || 1 || 2;
+console.log(test);
+//In this example, javascript only evaluates the false and the next value after that. Everything that comes after is completly ignored
+
+
+//REAL-LIFE EXAMPLE
+let userColor = 'red'; 
+let defaultColor = 'green'
+let currentColor = userColor || defaultColor;
+
+console.log(currentColor);
