cfecf147aabd3c24b9b1277ef43e51c37e7c8dbe
Johnpaul <indisputable.jay@gamil.com>
2023-05-27 18:56:31 +0100

    Addinng Index2.js

---
+++ Practising JavaScript/Operators/index2.js
@@ -0,0 +1,16 @@
+//BITWISE OPERATORS
+
+// 1 = 00000001 -> 8 bit -> 1 byte
+// 2 = 00000010  
+// 3 = 00000011
+
+console.log(1 || 2); // Bitwise OR 
+console.log(1 & 2); // Bitwise AND
+
+
+/*
+For the Bitwise OR calculation, if the binary notation of any of the operand is 1, the result prints 1, 
+or else it prints 0. Now the final result in binary from is then transfromed to decimal
+
+For the Bitwise AND calculation, same principles apply but both operands has to be 1 for the result to print out 1
+*/
