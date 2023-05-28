fe647511d40ab2e7013d84aa3726c90077bfbb94
Johnpaul Nnaji <indisputable.jay@gmail.com>
2023-05-28 13:21:55 +0100

    Alternatively...

--- Practising JavaScript/Control-flow/3-for.js
+++ Practising JavaScript/Control-flow/3-for.js
@@ -10,6 +10,6 @@ console.log('Hello, World')
 //Now this is ugly and unconventional. Theres an easier way of doing this
 
 for (let i = 0; i < 5; i++){
-    console.log('This is the power of loops')
+    console.log('This is the power of loops', i)
 }
 //This prints 'This is the power of loops' five times.
