c7fe035947db6d79a738883d71b429b850a89ac7
Johnpaul Nnaji <indisputable.jay@gmail.com>
2023-05-28 13:20:01 +0100

    for loops in action

--- Practising JavaScript/Control-flow/3-for.js
+++ Practising JavaScript/Control-flow/3-for.js
@@ -1,6 +1,15 @@
-//Iteration
+//Iteration or Loops
+//Lets print out the string 'Hello World' Five times
+
 console.log('Hello, World')
 console.log('Hello, World')
 console.log('Hello, World')
 console.log('Hello, World')
 console.log('Hello, World')
+
+//Now this is ugly and unconventional. Theres an easier way of doing this
+
+for (let i = 0; i < 5; i++){
+    console.log('This is the power of loops')
+}
+//This prints 'This is the power of loops' five times.
