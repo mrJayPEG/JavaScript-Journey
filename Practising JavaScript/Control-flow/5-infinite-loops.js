c7cde0560bdbc5bbb04c45f888ba514c8e56c3bd
Johnpaul Nnaji <indisputable.jay@gmail.com>
2023-05-29 10:03:02 +0100

    Another instance of an infintie loop

--- Practising JavaScript/Control-flow/5-infinite-loops.js
+++ Practising JavaScript/Control-flow/5-infinite-loops.js
@@ -2,7 +2,7 @@
 //These kinds of loops executes infinitely or forever and thiseither crashes your browser or your computer.
 
 let i = 0;
-while (i < 5){
-    console.log(i);
+do {
+    console.log('This is an infinite loop');
     //i++;
-}
+} while (i < 10);
