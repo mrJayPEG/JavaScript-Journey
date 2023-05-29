085a8cbe0d569ee35d264dda57c11d3e5734df46
Johnpaul Nnaji <indisputable.jay@gmail.com>
2023-05-29 10:00:49 +0100

    Infinite loops

---
+++ Practising JavaScript/Control-flow/5-infinite-loops.js
@@ -0,0 +1,8 @@
+//INFINITE LOOPS
+//These kinds of loops executes infinitely or forever and thiseither crashes your browser or your computer.
+
+let i = 0;
+while (i < 5){
+    console.log(i);
+    //i++;
+}
