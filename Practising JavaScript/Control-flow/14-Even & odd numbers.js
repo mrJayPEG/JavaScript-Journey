3040ba3b4720d0027644214e1ac2ecbd5f6d2c46
Johnpaul Nnaji <indisputable.jay@gmail.com>
2023-05-31 15:35:00 +0100

    Odd and Even Numbers exercise

---
+++ Practising JavaScript/Control-flow/14-Even & odd numbers.js
@@ -0,0 +1,11 @@
+//Write a function called shownumbers that takes a parameter called limits. 
+//so that when you call a number like 10, you should see all the numbers before 10 and weather they are even or odd
+
+function showNumbers(limits) {
+    if ((limits % 2) == 0)
+        console.log(limits, 'Even');
+    else
+        console.log(limits, 'Odd');
+}
+
+showNumbers(21245125);
