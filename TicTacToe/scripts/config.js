29d28d71b8118c20a8a105e531ae42c4e9f05d67
Johnpaul Nnaji <indisputable.jay@gmail.com>
2023-06-08 22:37:24 +0100

    Styling the overlay

--- TicTacToe/scripts/config.js
+++ TicTacToe/scripts/config.js
@@ -1,9 +1,28 @@
-function openPlayerConfig() {
-playerConfigOverlayElement.style.display = 'block';
-backdropElement.style.display = 'block'
+let editedPlayer = 0;
+
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid;
+    playerConfigOverlayElement.style.display = 'block';
+    backdropElement.style.display = 'block';
 }
 
-function closePlayerConfig(){
+function closePlayerConfig() {
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent = '';
+}
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername').trim(); // Gets player name and trim excess whitespace
+
+    if (!enteredPlayername) {
+        event.target.firstElementChild.classList.add('error');
+        errorsOutputElement.textContent = 'Please enter  valid name!';
+        return;
+    }
+
+
 }
