9e504400835fd29c073be03253fbfc309b4d7be1
Johnpaul Nnaji <indisputable.jay@gmail.com>
2023-06-13 15:06:19 +0100

    Optimising my game logic

--- TicTacToe/scripts/game.js
+++ TicTacToe/scripts/game.js
@@ -33,33 +33,59 @@ function selectGameField(event) {
 
 
     gameData[selectedRow][selectedColumn] = activePlayer + 1;
-    console.log(gameData);
 
+    const winnerid = checkForGameOver();
+    console.log(winnerid)
+    
+    currentRound++;
     switchPlayer();
 }
 
 function checkForGameOver() {
+
+    //checking the rows for equality
+    for (let i = 0; i <= 2; i++) {
+        if (
+            gameData[i][0] > 0 &&
+            gameData[i][0] === gameData[i][1] &&
+            gameData[i][1] === gameData[i][2]
+        ) {
+            return gameData[i][0];
+        }
+    }
+
+    //Checking the columns for equality
+    for (let i = 0; i <= 2; i++) {
+        if (
+            gameData[0][i] > 0 &&
+            gameData[0][i] === gameData[1][i] &&
+            gameData[0][1] === gameData[2][i]
+        ) {
+            return gameData[0][i];
+        }
+    }
+
+
+    //Checking diagonal from top yo bottom right
     if (
         gameData[0][0] > 0 &&
-        gameData[0][0] === gameData[0][1] &&
-        gameData[0][1] === gameData[0][2]
+        gameData[0][0] === gameData[1][1] &&
+        gameData[1][1] === gameData[2][2]
     ) {
         return gameData[0][0];
     }
 
+    //Checking diagonal from bottom left to top right
     if (
-        gameData[1][0] > 0 &&
-        gameData[1][0] === gameData[1][1] &&
-        gameData[1][1] === gameData[1][2]
-    ) {
-        return gameData[1][0];
-    }
-
-    if (
-        gameData[2][0] > 0 &&
-        gameData[2][0] === gameData[2][1] &&
-        gameData[2][1] === gameData[2][2]
+        gameData[0][0] > 0 &&
+        gameData[2][0] === gameData[1][1] &&
+        gameData[1][1] === gameData[0][2]
     ) {
         return gameData[2][0];
     }
+
+    if (currentRound === 9) {
+        return -1;
+    }
+    return 0;
 }
