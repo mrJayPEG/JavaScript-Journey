dccaf922f799a21c21991288bfda69688a9d0b4c
Johnpaul Nnaji <indisputable.jay@gmail.com>
2023-06-13 15:45:30 +0100

    TicTac-Toe

--- TicTacToe/scripts/app.js
+++ TicTacToe/scripts/app.js
@@ -7,6 +7,7 @@ const gameData = [
 let editedPlayer = 0;
 let activePlayer = 0;
 let currentRound = 1;
+let gameIsOver = false;  
 
 const players = [                                                   
     {
@@ -25,12 +26,14 @@ const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
 const gameAreaElement = document.getElementById('active-game');
 const activePlayerId = document.getElementById('active-player-name');
+const gameOverElement = document.getElementById('game-over');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnELement = document.getElementById('cancel-btn');
 const startNewGameBtnElement = document.getElementById('start-game-btn');
 const gameFieldElements = document.querySelectorAll('#game-board li');
+const gameBoardElement = document.getElementById('game-board')
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
--- TicTacToe/scripts/game.js
+++ TicTacToe/scripts/game.js
@@ -1,10 +1,32 @@
+function resetGameStatus() {
+    activePlayer = 0;
+    gameIsOver = false;
+    currentRound = 1;
+    gameOverElement.firstElementChild.innerHTML = 'You won! <span id="winner-name">PLAYER NAME';
+    gameOverElement.style.display = 'none'
+
+    let gameBoardIndex = 0;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            gameData[i][j] = 0;
+            const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+            gameBoardElement.children[gameBoardIndex].textContent = '';
+            gameBoardItemElement.classList.remove('disabled');
+            gameBoardIndex++;
+        }
+    }
+}
+
 function startNewGame() {
     if (players[0].name === '' || players[1].name === '') {
         alert('Please set names for both players to continue!')
         return;
     }
+
+    resetGameStatus();
     activePlayerId.textContent = players[activePlayer].name;
     gameAreaElement.style.display = 'block';
+
 }
 
 
@@ -18,6 +40,10 @@ function switchPlayer() {
 }
 
 function selectGameField(event) {
+
+    if (event.target.tagName !== 'LI' || gameIsOver){
+        return
+    }
 
     const selectedField = event.target;
     const selectedColumn = selectedField.dataset.col - 1;
@@ -34,9 +60,12 @@ function selectGameField(event) {
 
     gameData[selectedRow][selectedColumn] = activePlayer + 1;
 
-    const winnerid = checkForGameOver();
-    console.log(winnerid)
-    
+    const winnerId = checkForGameOver();
+
+    if (winnerId !== 0) {
+        endGame(winnerId);
+    }
+
     currentRound++;
     switchPlayer();
 }
@@ -88,4 +117,17 @@ function checkForGameOver() {
         return -1;
     }
     return 0;
+}
+
+function endGame(winnerId) {
+    gameIsOver = true;
+    gameOverElement.style.display = 'block';
+
+    if (winnerId > 0) {
+        const winnerName = players[winnerId - 1].name;
+        gameOverElement.firstElementChild.firstElementChild.textContent = winnerName;
+    }
+    else {
+        gameOverElement.firstElementChild.textContent = 'It\'s a draw!'
+    }
 }
